2c98008e4209f2c065f1e506bba42a7a9b4c5d1a
SmilingWolf <lupo996@gmail.com>
2019-03-02 09:55:04 +0100

    - update binaries
    - cjpeg now built with PNG support after an upstream fix

--- encode.ST.MozJPEG.lossy.sh
+++ encode.ST.MozJPEG.lossy.sh
@@ -26,8 +26,8 @@ heigth=$(head -1 "temp/orig_$filename.y4m" | grep -oE "\ H[0-9]{1,6}" | cut -b3-
 totalPixels=$(( $width * $heigth ))
 
 # Gotta play with the encoding parameters, this is pretty much an example
-./bins/ffmpeg -r 1 -y -hide_banner -loglevel fatal -i "$1" -vf $FFpad,scale=in_color_matrix=bt709:flags=lanczos+accurate_rnd+bitexact+full_chroma_int+full_chroma_inp,format=bgr24 -strict -1 "temp/orig_$filename.bmp"
-./bins/cjpeg -quality $quality $sampleSize -outfile "encoded/$filename.jpeg" "temp/orig_$filename.bmp"
+./bins/ffmpeg -r 1 -y -hide_banner -loglevel fatal -i "$1" -vf $FFpad,scale=in_color_matrix=bt709:flags=lanczos+accurate_rnd+bitexact+full_chroma_int+full_chroma_inp,format=bgr24 -strict -1 "temp/orig_$filename.png"
+./bins/cjpeg -quality $quality $sampleSize -outfile "encoded/$filename.jpeg" "temp/orig_$filename.png"
 cp "encoded/$filename.jpeg" "contained/$filename.jpeg"
 
 ./bins/ffmpeg -y -hide_banner -loglevel fatal -i "encoded/$filename.jpeg" -vf scale=out_color_matrix=bt709:flags=lanczos+accurate_rnd+bitexact+full_chroma_int+full_chroma_inp,format=$chromaFmt -strict -1 "temp/enc_$filename.y4m"
@@ -41,7 +41,7 @@ psnrhvs=$(./bins/dump_psnrhvs.exe -s "temp/orig_$filename.y4m" "temp/enc_$filena
 vmaf=$(./bins/vmafossexec.exe $chromaFmt $width $heigth "temp/orig_$filename.yuv" "temp/enc_$filename.yuv" ./bins/model/vmaf_v0.6.1.pkl | grep "score =" | cut -d" " -f4)
 echo "$1: Size=$filesize Pixels=$totalPixels PSNR=$psnr SSIM=$ssim PSNR-HVS-M=$psnrhvs MS-SSIM=$msssim VMAF=$vmaf" > "stats_$filename.txt"
 
-rm "temp/orig_$filename.bmp"
+rm "temp/orig_$filename.png"
 rm "temp/orig_$filename.y4m"
 rm "temp/orig_$filename.yuv"
 rm "temp/enc_$filename.y4m"
